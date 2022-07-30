3c66c7db51b149db2fa95e050f272d935a943f73
Kobashi Kazuki <kkobashi0701@gmail.com>
2022-07-30 15:16:14 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header } from "./layout/Header";
+
+export const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>Hello World</p>
+    </header>
+  </div>
+);
